02fce1138d669b5717ea557fd79594e8d80e8773
Daniel Cozar <daniel.cozar@cozardev.info>
2025-05-23 22:07:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`;
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
