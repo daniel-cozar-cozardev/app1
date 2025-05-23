50febddfc558e1c115482efb1eb190f7c3b752a2
Daniel Cozar <daniel.cozar@cozardev.info>
2025-05-23 23:41:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+oconst express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola desde Node.js y Docker!');
+  res.send('¡Hola desde Node.js y Docker! Esto es una prueba de despliegue continuo');
 });
 
 app.listen(port, () => {
